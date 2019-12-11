076c34f96207d5757e7bb017881aa5c0c5e2489f
Jacob Bremer <bremerjp@gmail.com>
2019-12-11 17:20:11 -0500

    completed mvp

---
+++ src/components/PhotoData.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import PhotoCard from "./PhotoCard";
+
+export default function PhotoData() {
+  const [photo, setPhoto] = useState([]);
+  const [date, setDate] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://api.nasa.gov/planetary/apod?api_key=zUQnuRDac7vc1HWP5T7cHmS9cDJuJUnFdfmgKigk`)
+      //&date=2014-07-14
+      .then(response => {
+        const nasaPhoto = response.data;
+        setPhoto(nasaPhoto);
+        console.log(nasaPhoto);
+      })
+      .catch(error => {
+        console.log("the data was not return", error);
+      });
+  }, []);
+
+  return (
+    <div className="photo-card">
+          
+          <PhotoCard 
+          key={photo.date}
+          title={photo.title}
+          date={photo.date}
+          description={photo.explanation}
+          imageUrl={photo.hdurl}
+          />
+    </div>
+  );
+}
