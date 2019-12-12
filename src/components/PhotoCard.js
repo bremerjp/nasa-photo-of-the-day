e81fc18c8d781fd684e8e12cf537420ad6065c08
Jacob Bremer <bremerjp@gmail.com>
2019-12-12 18:10:33 -0500

    added date picker and styling

--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -1,13 +1,35 @@
 import React from "react";
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button, Row, Form, FormGroup, Label, Input, FormText
+} from 'reactstrap';
 
 const PhotoCard = props => {
-    return (
-      <div className="photo-card" >
-        <img src={props.imageUrl} alt={props.title}/>
-        <h2>{props.title}</h2>
-        <p>Date: {props.date}</p>
-        <p>{props.description}</p>
-      </div>
-    );
-  };
-  export default PhotoCard;
+  const { imageUrl, title, date, description, changeDate, imageHDUrl } = props;
+
+  let i = '';
+
+  const change = event => {
+    i = event.target.value;
+  }
+
+  return (
+    <div>
+        <Card className="p-3 w-75 mx-auto">
+          <CardImg src={imageUrl} alt={title} />
+          <CardBody>
+            <CardTitle className="display-4 font-weight-bold">{title}</CardTitle>
+            <CardSubtitle className="text-muted">Date: {date}</CardSubtitle>
+            <CardText>{description}</CardText>
+            <Button onClick={() => window.open(imageHDUrl)}
+          className="button is-primary">View Full Image</Button>
+            <Form className="m-3">
+              <Input type="text" placeholder="YYYY-MM-DD" onChange={change} />
+              <Input type="submit" value="Change Date" onClick={e => changeDate(e, i)} />
+            </Form>
+          </CardBody>
+        </Card>
+    </div>
+  );
+};
+export default PhotoCard;
--- src/components/PhotoData.js
+++ src/components/PhotoData.js
@@ -1,30 +1,41 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import PhotoCard from "./PhotoCard";
+import date from 'date-and-time';
 
 export default function PhotoData() {
-  const [photo, setPhoto] = useState([]);
+  const [photo, setPhoto] = useState({});
+  const [today, setToday] = useState(date.format(new Date(), 'YYYY-MM-DD'));
+  
+  const changeDate = (e, i) => {
+    e.preventDefault();
+    setToday(i);
+  }
+
+  
   useEffect(() => {
     axios
-      .get(`https://api.nasa.gov/planetary/apod?api_key=zUQnuRDac7vc1HWP5T7cHmS9cDJuJUnFdfmgKigk`)
+      .get(`https://api.nasa.gov/planetary/apod?api_key=zUQnuRDac7vc1HWP5T7cHmS9cDJuJUnFdfmgKigk&date=${today}`) 
       .then(response => {
-        const nasaPhoto = response.data;
-        setPhoto(nasaPhoto);
-        console.log(nasaPhoto);
+        
+        setPhoto(response.data);
+        
       })
       .catch(error => {
         console.log("the data was not return", error);
       });
-  }, []);
+  }, [today]);
 
   return (
     <div className="photo-card">
-          <PhotoCard 
+          <PhotoCard
           key={photo.date}
           title={photo.title}
           date={photo.date}
           description={photo.explanation}
-          imageUrl={photo.hdurl}
+          imageUrl={photo.url}
+          imageHDUrl={photo.hdurl}
+          changeDate={changeDate}
           />
     </div>
   );
