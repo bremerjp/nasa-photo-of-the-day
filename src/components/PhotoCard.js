2c6a86acdd35caed54901ea794f42e0f51458bc1
Jacob Bremer <bremerjp@gmail.com>
2019-12-12 18:20:50 -0500

    updated styles

--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -1,34 +1,52 @@
 import React from "react";
 import {
-  Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, Button, Row, Form, FormGroup, Label, Input, FormText
-} from 'reactstrap';
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+  Form,
+  Input
+} from "reactstrap";
 
 const PhotoCard = props => {
   const { imageUrl, title, date, description, changeDate, imageHDUrl } = props;
 
-  let i = '';
+  let i = "";
 
   const change = event => {
     i = event.target.value;
-  }
+  };
 
   return (
     <div>
-        <Card className="p-3 w-75 mx-auto">
-          <CardImg src={imageUrl} alt={title} />
-          <CardBody>
-            <CardTitle className="display-4 font-weight-bold">{title}</CardTitle>
-            <CardSubtitle className="text-muted">Date: {date}</CardSubtitle>
-            <CardText>{description}</CardText>
-            <Button onClick={() => window.open(imageHDUrl)}
-          className="button is-primary">View Full Image</Button>
-            <Form className="m-3">
-              <Input type="text" placeholder="YYYY-MM-DD" onChange={change} />
-              <Input type="submit" value="Change Date" onClick={e => changeDate(e, i)} />
-            </Form>
-          </CardBody>
-        </Card>
+      <Card className="p-3 w-75 mx-auto">
+        <CardImg src={imageUrl} alt={title} />
+        <CardBody>
+          <CardTitle className="display-5 font-weight-bold">{title}</CardTitle>
+          <CardSubtitle className="text-muted">Date: {date}</CardSubtitle>
+          <CardText>{description}</CardText>
+          <Button onClick={() => window.open(imageHDUrl)} className="button">
+            View Full Image
+          </Button>
+          <Form className="m-3 w-25 mx-auto">
+            <Input
+              className="mt-3"
+              type="text"
+              placeholder="YYYY-MM-DD"
+              onChange={change}
+            />
+            <Input
+              className="mt-3"
+              type="submit"
+              value="Change Date"
+              onClick={e => changeDate(e, i)}
+            />
+          </Form>
+        </CardBody>
+      </Card>
     </div>
   );
 };
--- src/components/PhotoData.js
+++ src/components/PhotoData.js
@@ -1,25 +1,24 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import PhotoCard from "./PhotoCard";
-import date from 'date-and-time';
+import date from "date-and-time";
 
 export default function PhotoData() {
   const [photo, setPhoto] = useState({});
-  const [today, setToday] = useState(date.format(new Date(), 'YYYY-MM-DD'));
-  
+  const [today, setToday] = useState(date.format(new Date(), "YYYY-MM-DD"));
+
   const changeDate = (e, i) => {
     e.preventDefault();
     setToday(i);
-  }
+  };
 
-  
   useEffect(() => {
     axios
-      .get(`https://api.nasa.gov/planetary/apod?api_key=zUQnuRDac7vc1HWP5T7cHmS9cDJuJUnFdfmgKigk&date=${today}`) 
+      .get(
+        `https://api.nasa.gov/planetary/apod?api_key=zUQnuRDac7vc1HWP5T7cHmS9cDJuJUnFdfmgKigk&date=${today}`
+      )
       .then(response => {
-        
         setPhoto(response.data);
-        
       })
       .catch(error => {
         console.log("the data was not return", error);
@@ -28,15 +27,15 @@ export default function PhotoData() {
 
   return (
     <div className="photo-card">
-          <PhotoCard
-          key={photo.date}
-          title={photo.title}
-          date={photo.date}
-          description={photo.explanation}
-          imageUrl={photo.url}
-          imageHDUrl={photo.hdurl}
-          changeDate={changeDate}
-          />
+      <PhotoCard
+        key={photo.date}
+        title={photo.title}
+        date={photo.date}
+        description={photo.explanation}
+        imageUrl={photo.url}
+        imageHDUrl={photo.hdurl}
+        changeDate={changeDate}
+      />
     </div>
   );
 }
