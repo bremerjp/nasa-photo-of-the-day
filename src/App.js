21610779f075b97a6f05c7c7ff63c755e4234a2e
Jacob Bremer <bremerjp@gmail.com>
2019-12-12 19:10:26 -0500

    updated styles

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import React from "react";
 import "./App.css";
 import PhotoData from "./components/PhotoData";
 
-
-
 function App() {
   return (
-    <div style={{backgroundColor: '#f1f1f1'}} className="App p-3">
-      <h1>NASA Astronomy Picture of the Day</h1> 
+    <div style={{ backgroundColor: "#f1f1f1" }} className="App p-3">
+      <h1>NASA Astronomy Picture of the Day</h1>
       <PhotoData />
     </div>
   );
